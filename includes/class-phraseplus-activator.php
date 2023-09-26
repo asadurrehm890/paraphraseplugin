c9e97c94880252d366d533e82e96c5c73ea61e5a
asad890 <asadurrehm890@gmail.com>
2023-09-25 18:38:20 -0700

    update

---
+++ includes/class-phraseplus-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.urasaapi.com/about
+ * @since      1.0.0
+ *
+ * @package    Phraseplus
+ * @subpackage Phraseplus/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Phraseplus
+ * @subpackage Phraseplus/includes
+ * @author     Asad ur Rehman <asadurrehm890@gmail.com>
+ */
+class Phraseplus_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-phraseplus-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.urasaapi.com/about
+ * @since      1.0.0
+ *
+ * @package    Phraseplus
+ * @subpackage Phraseplus/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Phraseplus
+ * @subpackage Phraseplus/includes
+ * @author     Asad ur Rehman <asadurrehm890@gmail.com>
+ */
+class Phraseplus_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
